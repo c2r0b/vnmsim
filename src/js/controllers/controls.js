79107a184cf564804f2beed414ebf72eade37716
Lorenzo Ganni <lorenzoganni2@gmail.com>
2016-10-01 20:13:27 +0200

    Remove debug console log

--- src/js/controllers/controls.js
+++ src/js/controllers/controls.js
@@ -20,7 +20,6 @@ module.exports = ['$scope', 'sim', 'run', 'log', 'codeMirror',
       // update status and startup the simulation
       sim.status = status;
       run.begin($scope.delay);
-      console.log(status);
       log('LOG_RUNNING', ((status == 1) ? 'success' : 'step'));
     };
 
