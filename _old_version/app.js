9ce9e317ac0af3a5f660fa8c0c4336e7ccd21fa7
c2r0b <3254315+c2r0b@users.noreply.github.com>
2022-02-06 13:31:31 +0100

    Delete _old_version directory

--- _old_version/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    app = express();
-    path = require('path');
-
-app.use(express.static(__dirname + '/build'));
-
-var listener = app.listen(0);
-console.log('Express server listening on port ' + listener.address().port);
