e24c7cb40ed73fae8f25c46817888fdb6f94025a
c2r0b <c2r0b@proton.me>
2023-04-04 00:46:05 +0200

    fix nextjs i18n with tauri

--- next.config.js
+++ next.config.js
@@ -2,25 +2,10 @@ const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
-const { tx } = require('@transifex/native');
-
-tx.init({
-  token: process.env.TX_NATIVE_PUBLIC_TOKEN,
-});
-
 module.exports = async () => withPWA({
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  i18n: {
-    locales: ['default', ...(
-      await tx.getLanguages())
-        .filter((lang) => lang.code !== 'en')
-        .map((lang) => lang.code.replace('_', '-').toLocaleLowerCase())
-    ],
-    defaultLocale: 'default',
-    localeDetection: true,
   },
   publicRuntimeConfig: {
     TxNativePublicToken: process.env.TX_NATIVE_PUBLIC_TOKEN,
