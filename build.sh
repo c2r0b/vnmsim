f3f5cec5c2135a71c0d34be6412721559ec99c76
c2r0b <c2r0b@proton.me>
2023-11-17 15:25:12 +0100

    commands cleanup

--- build.sh
+++ build.sh
@@ -9,3 +9,6 @@ curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 echo "Building wasm-parser..."
 # Build wasm-parser 
 npm run build:wasm
+# Build next
+echo "Building next..."
+npm run build:next
